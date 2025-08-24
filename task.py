72556740d57b718dc812607d204f954db9b56624
kprince9289-droid <kprince9289@gmail.com>
2025-08-24 10:16:58 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,17 @@
+#task1 basic mathematical operators :
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+print("Addition: ", addition)
+print("Subtraction: ", subtraction)
+print("Multiplication: ", multiplication)
+print("Division: ", division)
+
+# task2 personalize greeting
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+full_name = f"{first_name} {last_name}"
+print(f"\nhello {full_name}! welcome to the python program.")

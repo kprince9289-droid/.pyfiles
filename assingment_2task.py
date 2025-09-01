b653998a35ef88be0566ba8d41365c6f45c9090e
kprince9289-droid <kprince9289@gmail.com>
2025-09-01 21:18:40 +0530

    Add files via upload

---
+++ assingment_2task.py
@@ -0,0 +1,17 @@
+# task 1
+
+num = int(input("Enter a number: "))
+
+if num % num == 0:
+    print("The number is an even number.")
+else:
+    print("The number is an odd number.")
+
+# task 2
+
+total = 0
+for i in range(1, 51):
+    total += i
+
+print("The sum of numbers from 1 to 50 is:", total)
+
